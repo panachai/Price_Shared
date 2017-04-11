c9f6eae2cb7b8025856a176600c890c369dbae48
Panachai <panachai.ny@gmail.com>
2017-04-11 23:47:04 +0700

    delete activity not use

--- app/src/main/java/com/panachai/priceshared/MainActivity.java
+++ app/src/main/java/com/panachai/priceshared/MainActivity.java
@@ -1,52 +1,15 @@
 package com.panachai.priceshared;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.EditText;
-
 public class MainActivity extends AppCompatActivity {
-/*EditText UsernameEt, PasswordEt;
-*/
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-/*
-        UsernameEt = (EditText) findViewById(R.id.etUserName);
-        PasswordEt = (EditText) findViewById(R.id.etPassword);
-*/
-    }
-/*
-    public void OnLogin(View view) {
-        String username = UsernameEt.getText().toString();
-        String password = PasswordEt.getText().toString();
-        String type = "login";
-
-
-        //Login login = new Login(this);
-        //login.execute(type, username, password);
-
-        DBHelper dbhelper = new DBHelper(this);
-        dbhelper.execute(type, username, password);
 
     }
-
-    public void OnRegis(View view) {
-
-        //String username = UsernameEt.getText().toString();
-        //String password = PasswordEt.getText().toString();
-        //Register register = new Register(this);
-        //register.execute(username, password);
-
-
-        Intent intent = new Intent(this, RegisterActivity.class);
-        startActivity(intent);
-
-
-    }
-*/
 
 }
